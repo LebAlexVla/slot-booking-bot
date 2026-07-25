9c5b5a079045fc556a2f10efd07371adbdb73afd
Alexander Lebedev <lebalexvla@gmail.com>
2026-07-25 20:43:24 +0300

    chore: init project structure

---
+++ src/main/kotlin/ru/lebalexvla/slotbookingbot/SlotBookingBotApplication.kt
@@ -0,0 +1,11 @@
+package ru.lebalexvla.slotbookingbot
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SlotBookingBotApplication
+
+fun main(args: Array<String>) {
+    runApplication<SlotBookingBotApplication>(*args)
+}
---
+++ src/test/kotlin/ru/lebalexvla/slotbookingbot/SlotBookingBotApplicationTests.kt
@@ -0,0 +1,13 @@
+package ru.lebalexvla.slotbookingbot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SlotBookingBotApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
